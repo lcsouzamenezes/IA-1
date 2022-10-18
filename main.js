6052efa1e043318a8825847ba60104c7e4df87ad
Apolo Oxosse Santoni <34612177+apolosantoni@users.noreply.github.com>
2022-10-18 14:30:51 -0300

    Update main.js
    alterando de 1 para 100 carros.

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ const carCtx = carCanvas.getContext('2d');
 const road = new Road(carCanvas.width / 2, carCanvas.width * 0.9);
 /*const car = new Car(road.getLaneCenter(1), 100, 30, 50, "AI");*/
 
-const N = 2;
+const N = 100;
 const cars = generateCars(N);
 let bestCar = cars[0];
 if (localStorage.getItem("bestBrain")) {
